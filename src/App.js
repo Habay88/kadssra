0ad014b3448abe7aa4ae74e5475bc4140282af96
HabaySecureId <92311631+HabaySecureId@users.noreply.github.com>
2022-07-03 01:11:55 +0100

    RENDERING JSON DATA

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React,{Component} from 'react';
-//import testrun from './jsondata/testrun';
+ import ReactHTML from 'react-html-to-excel';
 import axios from 'axios';
 import { testrun } from './jsondata/testrun';
+import {kadnin} from './jsondata/kadnin';
 
 var allDetails =  [];
-class App extends Component {
+export default class App extends Component {
   state = {
     clients: [], 
   };
@@ -17,7 +19,7 @@ class App extends Component {
   }
 
    getDetails(){
-    testrun.map(i =>{
+    kadnin.map(i =>{
      var  id = i.NIN
       axios.get("https://kadsrramiddlewaredev.azurewebsites.net/api/Enrollments/get-kadsrrainfo/"+ id)
       .then(res => {
@@ -29,22 +31,62 @@ class App extends Component {
 }
 
   render() {
-    const {clients} = this.state;
+    
     return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <div className="App-intro">
-              <h2>KADSSRA DETAILS</h2>
-              {clients.map(client =>
-                  <div key={client.id}>
-                    {client.name} ({client.email})
-                  </div>
-              )}
-            </div>
-          </header>
+      <>
+       
+        
+          <h2 className='text-center text-info mt-3'>KADSSRA DETAILS</h2>
+      <section className='py-4 container'>
+        <div className='row justify-content-center'>
+          <div className='col-12'>
+        <ReactHTML ToExcel className="btn btn-info" table="emp-table" filename="KADSSRA EXCEL FILE" sheet="Sheet" buttonText="Export to Excel" />
         </div>
-    );
-  }
-}
-export default App;
+          <table className='table table-striped' id='emp-table'>
+            <thead>
+              <tr>
+            <td>Name</td>
+            <td>NIN</td>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Full Name</td>
+            <td>DOB</td>
+            <td>Gender</td>
+            <td>Telephone</td>
+            <td>Photo</td>
+            <td>KADSSRA ID</td>
+            <td>Middle Name</td>
+            <td>Other Name</td>
+            <td>Maiden Name</td>
+            <td>Email</td>
+            <td>Title</td>
+            <td>Aztec code</td>
+              </tr>
+            </thead>
+            <tbody>   
+          {kadnin.map((item, i ) => (
+            <tr key={i}>
+              <td>{item.NAME}</td>
+              <td>{item.NIN}</td>
+              <td>{item.fullName}</td>
+              <td>{item.dob}</td>
+              <td>{item.gender}</td>
+              <td>{item.telephone}</td>
+              <td>{item.photo}</td>
+              <td>{item.kadsrraId}</td>
+              <td>{item.middleName}</td>
+              <td>{item.otherName}</td>
+              <td>{item.maidenName}</td>
+              <td>{item.email}</td>
+              <td>{item.title}</td>
+              <td>{item.aztecCode}</td>
+            </tr>
+         ))}
+      </tbody>
+          </table>
+     
+      </div>
+      </section>
+</>
+  );     
+}}
